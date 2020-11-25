8787131b30655411ec9d302166d067661e605893
Heger Arias Santos <Hegel.0218@gmail.com>
2020-11-25 00:20:26 -0400

    fixing typos

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,9 @@
-const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
-   "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z", " ", ",", "!" ,"?",";",":","'","1","2","3","4","5","6","7","8","9","á","é","í","ó","ú", "ñ",".","\n"];
-  //  arreglar el problema de que los saltos de line lo coje como underfined
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j","k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z", " ", ",", "!" ,"?",";",":","'","1","2","3","4","5","6","7","8","9","á","é","í","ó","ú", "ñ",".","\n"];
   function encrypt(text) {
     let change = text.trim()
     const changedText = change.toLowerCase();
     let newString = "";
     Array.prototype.forEach.call(changedText, letra => {
-      //  if(!(/[a-zA-Z," ",",",";",":","'"]/).test(letra)) {
-      //  return;
-      //  } 
        if (alphabet.indexOf(letra) < 9){
         let index =  `0${alphabet.indexOf(letra)  +  1}`
         newString += index + " ";
@@ -22,7 +17,6 @@ const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
     })
     return newString.trim();
   }
-
 function Decrypt(number) {
   let textoFinal = "";
   let numberS = number.trim()
@@ -37,36 +31,32 @@ function Decrypt(number) {
   }
   return textoFinal
 }
-// let CambiarAletras = (numeros) => numeros.toString()
 let Cod__text2 = document.getElementById("Cod__text")
 let Cod__number2 = document.getElementById("Cod__number")
-
 let translateText = () =>{
   setTimeout(() => {
-  console.log('text')
-  console.log(encrypt(Cod__text2.value))
+  console.log('text being encoding')
   return Cod__number2.value = encrypt(Cod__text2.value)
   }, 800);
-  return Cod__number2.value = "Traduciendo..."
-  
+  return Cod__number2.value = "Translating..."
 }
 let translateNumber = () =>{
    setTimeout(() => {
      if (Cod__text2.value == "" && Cod__number2.value == "") {
-       console.log('text empyte')
+       console.log('text empty')
      } else if (Cod__text2.value = " "  && Cod__number2.value == "") {
-       console.log('vacio')
+       console.log('empty')
        return Cod__text2.value = ""
      } else {
-     console.log('number')
-     console.log( Decrypt(Cod__number2.value))
+     console.log('number being decoding')
      return Cod__text2.value = Decrypt(Cod__number2.value)
      }
    }, 800);
  
-  return Cod__text2.value = "Traduciendo..."
+  return Cod__text2.value = "Translating..."
   
 }
+
 
 
 
